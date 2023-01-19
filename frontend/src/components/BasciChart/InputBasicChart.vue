7134aaeca2faea44f0d6e9c428ebe1060252ae2a
kwonjelly <goodsosbva@naver.com>
2023-01-19 17:25:40 +0900

    final!

--- frontend/src/components/BasciChart/InputBasicChart.vue
+++ frontend/src/components/BasciChart/InputBasicChart.vue
@@ -47,6 +47,35 @@ interface OptionTypes {
   };
 }
 
+interface TooltipItem {
+  // The chart the tooltip is being shown on
+  chart: Chart;
+
+  // Label for the tooltip
+  label: string;
+
+  // Parsed data values for the given `dataIndex` and `datasetIndex`
+  parsed: object;
+
+  // Raw data values for the given `dataIndex` and `datasetIndex`
+  raw: object;
+
+  // Formatted value for the tooltip
+  formattedValue: string;
+
+  // The dataset the item comes from
+  dataset: object;
+
+  // Index of the dataset the item comes from
+  datasetIndex: number;
+
+  // Index of this data item in the dataset
+  dataIndex: number;
+
+  // The chart element (point, arc, bar, etc.) for this tooltip item
+  element: Element;
+}
+
 const datas = shallowRef<any>({
   data: {
     datasets: [
@@ -77,8 +106,8 @@ const options = shallowRef<OptionTypes>({
   plugins: {
     tooltip: {
       callbacks: {
-        title: function (context: []) {
-          if (context[0].length > 0 && context) {
+        title: function (context: TooltipItem[]) {
+          if (context.length > 0) {
             if (context[0].datasetIndex === 0) {
               console.log(context);
               return context[0].label;
